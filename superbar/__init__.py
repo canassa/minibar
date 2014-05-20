e9f7b46f04ab7eebbe9bcedc14d1e70577f37474
Cesar Canassa <cesar.canassa@gmail.com>
2014-05-20 18:22:52 -0300

    Calculating the terminal total width

--- superbar/__init__.py
+++ superbar/__init__.py
@@ -16,27 +16,28 @@ if PY2:
 else:
     text_type = str
 
-COLORS = {c: '\033[9{0}m'.format(i) for i, c in enumerate(['black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white'])}
+COLORS = {c: '\033[3{0}m'.format(i) for i, c in enumerate(['black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white'])}
 COLORS['r'] = '\033[0m'
 
 
 def get_terminal_width():
     rows, columns = os.popen('stty size', 'r').read().split()
-    return columns
+    return int(columns)
 
 
-def iprint(string):
-    print('\r' + text_type(string).format(**COLORS), file=sys.stdout, end='')
+def iprint(string, *args):
+    print('\r' + text_type(string).format(*args, **COLORS), file=sys.stdout, end='')
     sys.stdout.flush()
 
 
 def bar(iterator):
     total = len(iterator)
-    # template = '▏▎▍▌▋▊▉█'
+    terminal_width = get_terminal_width() - 2
     template = '▏▎▍▌▋▊▉'
     for counter, i in enumerate(iterator):
-        counter += 1
-        output = template[-1] * (counter // len(template))
-        output += template[counter % len(template)]
-        iprint('{:100}'.format(output))
+        tmp = (((counter * 100.0) / total) * terminal_width) / 100
+        output = template[-1] * int(tmp)
+        output += template[int((tmp % 1) * len(template))]
+        iprint('{red}[{green}{:' + text_type(terminal_width) + '}' + '{red}]{r}', output)
+
         yield i
