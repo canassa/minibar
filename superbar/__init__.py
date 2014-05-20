854cc626ed94b71efd2c9bca84ae2a43cd6db7a5
Cesar Canassa <cesar.canassa@gmail.com>
2014-05-20 16:24:10 -0300

    Supporting Python 2 and 3

--- superbar/__init__.py
+++ superbar/__init__.py
@@ -1,9 +1,20 @@
-__version__ = '0.1.dev1'
-
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from __future__ import unicode_literals
 import sys
 import os
 
+__version__ = '0.1.dev1'
 __all__ = ['bar']
+
+
+PY2 = sys.version_info[0] == 2
+PY3 = sys.version_info[0] == 3
+
+if PY2:
+    text_type = unicode
+else:
+    text_type = str
 
 COLORS = {c: '\033[9{0}m'.format(i) for i, c in enumerate(['black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white'])}
 COLORS['r'] = '\033[0m'
@@ -15,7 +26,7 @@ def get_terminal_width():
 
 
 def iprint(string):
-    print('\r' + str(string).format(**COLORS), file=sys.stdout, end='')
+    print('\r' + text_type(string).format(**COLORS), file=sys.stdout, end='')
     sys.stdout.flush()
 
 
